98d6982543d292adc3bec65c665c5bff422f2304
Ed Mamedov <em524@scarletmail.rutgers.edu>
2015-02-12 16:20:00 -0500

    returns arraylist of song objects from input file

--- src/songLib/SongPanel.java
+++ src/songLib/SongPanel.java
@@ -4,9 +4,12 @@
  */
 package songLib;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
@@ -19,29 +22,58 @@ import javax.swing.ListSelectionModel;
  */
 
 public class SongPanel extends JPanel{
-	
+
 	private JList songlist;
-    private DefaultListModel listModel;
-    private ArrayList<Song> songs;
-    
+	private DefaultListModel listModel;
+	private ArrayList<Song> songs;
+	String name, artist, year, album;
+
 	public SongPanel(ArrayList<Song> songs){
 		this.songs = songs;
 		makeList();
 	}
-	
+
+	public static ArrayList<Song> readFile(){
+		Scanner filereader;
+		Song song = null;
+		ArrayList<Song> songArray = new ArrayList<Song>();
+		try{
+			FileInputStream fstream = new FileInputStream("/Users/Petrov/Desktop/test.txt");
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			while ((strLine = br.readLine()) != null)   {
+				String[] tokens = strLine.split(" ");
+				if (tokens[2] == null){
+					tokens[2] = "";
+				}
+				if (tokens[3] == null){
+					tokens[3] = "";
+				}
+				songArray.add(new Song(tokens[0],tokens[1],tokens[2],tokens[3]));
+			}
+			in.close();
+		}catch (Exception e){
+			System.err.println("Error: " + e.getMessage());
+		}
+
+		return songArray;
+	}
+
 	private void makeList(){
-		
+
 		listModel = new DefaultListModel();
 		for(int i = 0; i < songs.size(); i++){
 			listModel.addElement(songs.get(i).name);
 		}
-		
+
 		//JList doesn't have scroll
 		songlist = new JList(listModel);
 		songlist.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		songlist.setLayoutOrientation(JList.VERTICAL);
 		songlist.setSelectedIndex(0);
-		songlist.setVisibleRowCount(5);
+		songlist.setVisibleRowCount(12);
+		songlist.setPrototypeCellValue("111111111111111111111111111111");
 		JScrollPane pane = new JScrollPane(songlist);
 		add(pane);
 	}
